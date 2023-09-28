b94029ae213750bb08edce13a7c8249d45dc2529
Anastasya Azami <80338392+Tasyul@users.noreply.github.com>
2023-09-28 20:21:41 +0700

    Add files via upload

--- PBO_41822010081_LATIHAN25.java
+++ PBO_41822010081_LATIHAN25.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
-
+/**
+ * @author nstsy
+ * Nama : Anastasya azami
+ * NIM : 41822010081
+ * Nama : Syadella Putri Ardana
+ * NIM: 41822010037
+ */
 public class PBO_41822010081_LATIHAN25 {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
